fbee3a3f5c3196b1ea21fbcf509181e37f0ead51
Olivia-Codes <fostero@mail.gvsu.edu>
2024-10-17 19:06:31 -0400

    Add files via upload

---
+++ src/everythingbutmain/FunkyFeatures.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Artifacts:
+    def __init__(self, maingame):
+        self.maingame = maingame
+        self.artifacts_not_picked_up = {}
+        self.artifacts_picked_up = {}
+
+        pass
+
+
+
+class NPCs:
+    def __init__(self, maingame, avatar):
+        self.maingame = maingame
+        self.avatar = avatar
+
+        pass

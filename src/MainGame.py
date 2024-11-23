38267d30f9150901d25effaa1ba104215f0b6558
Olivia-Codes <fostero@mail.gvsu.edu>
2024-11-23 16:42:06 -0500

    Update MainGame.py

--- src/MainGame.py
+++ src/MainGame.py
@@ -34,7 +34,7 @@ PHYSICS_TILES = {'grass', 'stone'}
 
 
 class Adventure:
-    def __init__(self):
+    def __init__(self, currentMap='map.json'):
         pygame.init()
         pygame.display.set_caption('Dog\'s Journey Home')
 
@@ -48,11 +48,7 @@ class Adventure:
         self.channel3 = pygame.mixer.Channel(2)
         self.channel3.set_volume(0.2)
 
-        #self.channel2.play(pygame.mixer.Sound('artifacts/artifacts_falling.mp3'))
-
-
-
-        #pygame.mixer.music.play(-1)
+       
 
         self.tile_dimension = 16
         self.tile_layout = {}
@@ -62,39 +58,47 @@ class Adventure:
         self.character_type = 'avatar'
         self.timer = pygame.time.Clock()
         self.movement_status = [False, False]
-        self.resources = {
+         self.resources = {
             'decor': load_pictures('tiles/decor'),
             'grass': load_pictures('tiles/grass'),
             'large_decor': load_pictures('tiles/large_decor'),
             'stone': load_pictures('tiles/stone'),
             'player': load_picture('entities/player/player.png'),
             'background': load_picture('background.jpg'),
+            'forest-background': load_picture('forest-background.png'),
             'water': load_pictures('tiles/water'),
             'artifacts': load_pictures('artifacts'),
             'player/thing': AnimationSequence(load_pictures('entities/player/thing')),
             'player/run': AnimationSequence(load_pictures('entities/player/run'), 4),
-            'player/idle': AnimationSequence(load_pictures('entities/player/idle'), 4), 
-            'player/jump': AnimationSequence(load_pictures('entities/player/jump'), 4),
-            'NPC/tomato': load_pictures('entities/NPC/tomato'),
+            'NPC/tomato': AnimationSequence(load_pictures('entities/NPC/tomato')),
+            'NPC/Chipmunk': AnimationSequence(load_pictures('entities/NPC/Chipmunk')),
             'float': load_pictures('float'),
             'ghost/down' : AnimationSequence(load_pictures('entities/ghost/down'), 0.5),
             'ghost/up': AnimationSequence(load_pictures('entities/ghost/up'), 0.5),
             'ghost/left': AnimationSequence(load_pictures('entities/ghost/left'), 0.5),
-            'ghost/right': AnimationSequence(load_pictures('entities/ghost/right'),0.5)
+            'ghost/right': AnimationSequence(load_pictures('entities/ghost/right'),0.5),
+            'seaMonster' : load_picture('entities/seaMonster/0.png')
 
 
         }
-        self.load_game('src/map.json')
+        self.load_game('src/' + self.currentMap)
         self.scroll_offset = [0, 0]
         self.avatar = Avatar(self, 'player', (0, 0), (10, 10), 'thing')
         self.artifacts = Artifacts(self)
-        self.tomato = NPCs(self, 'NPC/tomato')
+        self.chipmunk = NPCs(self, 'NPC/Chipmunk', (742, 6), (16, 16))
+        #[742, 6]
+        #[419, 86.1]
+        self.tomato = NPCs(self, 'NPC/tomato', (419, 86.1), (16,16))
         self.current_animation = self.resources['player/thing'].duplicate()
+        self.current_animation_Chipmunk = self.resources['NPC/Chipmunk'].duplicate()
+        self.current_animation_tomato = self.resources['NPC/tomato'].duplicate()
         self.current_action = 'thing'
-        self.message = NPCMessage(self, 419, 20, False)
+        if self.currentMap == 'map.json':
+            self.message = NPCMessage(self, 419, 20, False)
+        else:
+            self.message = NPCMessage(self, 770, -20, False)
         self.ghosts = Ghosts(self)
         self.obstacle = ObstacleFloat(self, 'float')
-        self.game_completed = False #for congratulation banner
 
 
     def surrounding_tiles(self, position):
@@ -105,6 +109,24 @@ class Adventure:
             if check_position in self.tile_layout:
                 tiles.append(self.tile_layout[check_position])
         return tiles
+        
+     def display_congratulations(self, screen):
+        """Render the congratulations banner."""
+        banner_rect = pygame.Rect(0, 0, screen.get_width(), 100)
+        banner_rect.center = (screen.get_width() // 2, screen.get_height() // 2)
+
+        # Draw banner background
+        pygame.draw.rect(screen, pygame.Color('gold'), banner_rect)
+        pygame.draw.rect(screen, pygame.Color('black'), banner_rect, 5)  # Border
+
+        # Render text
+        font = pygame.font.SysFont('Arial', 15, bold=True)
+        text_surface = font.render("Congratulations! You reached home!", True, pygame.Color('black'))
+        text_rect = text_surface.get_rect(center=banner_rect.center)
+
+        # Display text
+        screen.blit(text_surface, text_rect)
+       #To call later : self.display_congratulations(self.render_surface)
 
     def save_game(self, file):
         f = open(file, 'w')
@@ -144,29 +166,39 @@ class Adventure:
                                  (tile['pos'][0] * self.tile_dimension - offset[0],
                                   tile['pos'][1] * self.tile_dimension - offset[1]))
 
+
     def run(self):
         while True:
-
-            self.render_surface.blit(self.resources['background'], (0, 0))
+            if self.currentMap == 'map.json':
+                self.render_surface.blit(self.resources['background'], (0, 0))
+            else:
+                self.render_surface.blit(self.resources['forest-background'], (0, 0))
             # self.render_surface.blit(self.resources['artifacts'][0], (300,110))
             #print(self.avatar.position[0], self.avatar.position[1])
             if self.avatar.avatar_velocity[1] > 2.5:
-                
+
                     # Check if the sound is not already playing
                 if not self.channel3.get_busy():
                     self.channel3.play(pygame.mixer.Sound('artifacts/artifacts_falling.mp3'))
 
-            print(self.avatar.avatar_velocity)
+            #print(self.avatar.position)
             if self.avatar.position[1] >= 500:
-                self.game_completed = True
-                Adventure().run()
+                Adventure(self.currentMap).run()
             self.scroll_offset[0] += (self.avatar.rect().centerx - self.render_surface.get_width() / 2 -
                                       self.scroll_offset[0]) / 30
             self.scroll_offset[1] += (self.avatar.rect().centery - self.render_surface.get_height() / 2 -
                                       self.scroll_offset[1]) / 30
             render_scroll = (int(self.scroll_offset[0]), int(self.scroll_offset[1]))
             self.obstacle.drawObstacle(self.render_surface, distanceFromCamera=render_scroll)
-            self.tomato.drawNPC(self.render_surface, distanceFromCamera=render_scroll)
+            if self.currentMap == 'map.json':
+
+                self.tomato.update_NPC()
+                self.tomato.render(self.render_surface, offset=render_scroll)
+            else:
+                self.chipmunk.update_NPC()
+                self.chipmunk.render(self.render_surface, offset=render_scroll)
+
+            #self.tomato.drawNPC(self.render_surface, distanceFromCamera=render_scroll)
             self.render(self.render_surface, offset=render_scroll)
             self.avatar.update_avatar(self.tile_layout, (self.movement_status[1] - self.movement_status[0], 0))
             self.avatar.render(self.render_surface, offset=render_scroll)
@@ -175,19 +207,18 @@ class Adventure:
             if self.tomato.check_collision_with_NPC(self.avatar.rect(), self.render_surface,
                                                     distanceFromCamera=render_scroll):
                 self.message.drawMessage(self.render_surface, distanceFromCamera=render_scroll)
+
+                if self.artifacts.canDogMoveOn and not self.currentMap == 'map2.json':
+
+                    Adventure('map2.json').run()
+
+
             pygame.display.update()
             self.artifacts.check_collision_with_artifacts(self.avatar.rect())
             self.ghosts.draw_ghosts(self.render_surface, distanceFromCamera=render_scroll)
             self.ghosts.check_collision_with_ghosts(self.avatar.rect())
             if self.ghosts.check_collision_with_ghosts(self.avatar.rect()):
-                Adventure().run()
-
-            if self.game_completed:
-                self.display_congratulations(self.render_surface)
-                pygame.display.update()
-                pygame.time.wait(3000)  # Pause for 3 seconds
-                pygame.quit()
-                sys.exit()
+                Adventure(self.currentMap).run()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -210,24 +241,6 @@ class Adventure:
             pygame.display.update()
 
             self.timer.tick(60)
-    def display_congratulations(self, screen):
-        """Render the congratulations banner."""
-        banner_rect = pygame.Rect(0, 0, screen.get_width(), 100)
-        banner_rect.center = (screen.get_width() // 2, screen.get_height() // 2)
-
-        # Draw banner background
-        pygame.draw.rect(screen, pygame.Color('gold'), banner_rect)
-        pygame.draw.rect(screen, pygame.Color('black'), banner_rect, 5)  # Border
-
-        # Render text
-        font = pygame.font.SysFont('Arial', 15, bold=True)
-        text_surface = font.render("Congratulations! You reached home!", True, pygame.Color('black'))
-        text_rect = text_surface.get_rect(center=banner_rect.center)
-
-        # Display text
-        screen.blit(text_surface, text_rect)
 
 
 Adventure().run()
-
-
